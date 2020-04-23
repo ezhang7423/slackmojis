da94d31d513a1362c00fa5b51ad490471ba2575e
ezhang7423 <ezhang7423@student.palomar.edu>
2020-04-23 15:01:28 -0700

    add driver

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+# from scraper import * shous stuff
+
+
+def prompt(query):
+    y = input(query)
+    while y != 'y' and y != 'n':
+        print("That's not a valid option.")
+        y = input(query)
+    return y
+
+y = prompt("Have you set credentials yet? (y/n)")
+if y == 'n':
+    ss = input("What's the url of your slack site?")
+    email = input("What's your email?")
+    password = input("What's your password? This information stays only on your computer.")
+    with open('uploader/.env', 'w') as fout:
+        fout.write(f"SLACKSITE={ss}\n")
+        fout.write(f"EMAIL={email}\n")
+        fout.write(f"PASSWORDD={password}\n")
+
+
+
+os.system('python3 -mwebbrowser https://slackmojis.com/')
+y = prompt("Would you like to download off of the main page?")  
+if y == 'y':
+    y = prompt("Which header would you like to download from?")
+    # call shous super cool api
+    # if gifs is not empty, move all files into new folder gifbak
+else: 
+    y = prompt("which category would you like to download from?")
+    # call shous amazing api
+
+
+## all data should be downloaded into gifs folder at this point
+os.chdir('uploader')
+os.system('python3 script.py')
